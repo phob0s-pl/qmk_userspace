4bd5bf4ab05c73f98f17688a26a3194b9ad9d1d5
phob0s <phob0s@phob0s.pl>
2025-04-17 13:38:09 +0200

    phob0s variant

---
+++ keyboards/splitkb/halcyon/kyria/keymaps/phob0s/rules.mk
@@ -0,0 +1,12 @@
+ENCODER_MAP_ENABLE = no
+# This adds module functionality to your keyboard (files found in users/halcyon_modules)
+USER_NAME := halcyon_modules
+
+
+# Include the display.c file during compilation
+#SRC += display.c
+#SRC += graphics/thintel15.qff.c
+
+
+
+POINTING_DEVICE_DRIVER = cirque_pinnacle_i2c
